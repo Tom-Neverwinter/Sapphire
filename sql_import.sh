ac2f124f3be3e81445478f3008ea1df6ced07090
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2019-02-16 23:46:55 -0600

    small changes
    added pause and password field

--- sql_import.sh
+++ sql_import.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd bin
-./dbm --mode initialize --user root --database sapphire --sfile sql/schema/schema.sql --ifile sql/schema/inserts.sql
-./dbm --mode liquidate --user root --database sapphire --force
+./dbm --mode initialize --user root --pass password --database sapphire --sfile sql/schema/schema.sql --ifile sql/schema/inserts.sql
+./dbm --mode liquidate --user root --pass password --database sapphire --force
 cd ..
+
+read -p "Press enter to continue"
